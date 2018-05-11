2039ad4eda95bc47f63cfccc4748641b49bcecab
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2018-05-11 18:53:48 +0700

    fix redirect path on login page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -125,7 +125,7 @@ class SecurityController extends AbstractController
         $referer = $request->getServerParam('HTTP_REFERER');
 
         if ((\stripos($host, $this->domain) === false) || ($referer === null)) {
-            return $this->router->pathFor('files');
+            return $this->router->pathFor('files-root');
         }
 
         /** @var string $refererPath */
